c570a28554f4e6b7541d5dd7fb5adeb7b620791a
dhruthi.06 <dhruthi.06@gmail.com>
2025-11-22 04:56:22 -0500

    all changes

--- Kambaz/Courses/dao.js
+++ Kambaz/Courses/dao.js
@@ -1,17 +1,11 @@
 import { v4 as uuidv4 } from "uuid";
 
 export default function CoursesDao(db) {
-
-
-
-  const findAllCourses = () => {
-    return db.courses;
-  };
+  const findAllCourses = () => db.courses;
 
   const findCoursesForEnrolledUser = (userId) => {
-    const { courses, enrollments } = db;
-    return courses.filter((course) =>
-      enrollments.some(
+    return db.courses.filter((course) =>
+      db.enrollments.some(
         (en) => en.user === userId && en.course === course._id
       )
     );
@@ -25,14 +19,14 @@ export default function CoursesDao(db) {
 
   const deleteCourse = (courseId) => {
     db.courses = db.courses.filter((c) => c._id !== courseId);
+    db.enrollments = db.enrollments.filter((e) => e.course !== courseId);
     return true;
   };
 
   const updateCourse = (courseId, updates) => {
-    db.courses = db.courses.map((c) =>
-      c._id === courseId ? { ...c, ...updates } : c
-    );
-    return db.courses.find((c) => c._id === courseId);
+    const course = db.courses.find((c) => c._id === courseId);
+    Object.assign(course, updates);
+    return course;
   };
 
   return {
